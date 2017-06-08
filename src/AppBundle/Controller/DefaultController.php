d4cf37f50d351556ebc3b98ea5b039faabfe1576
walid bousnina <bousnina.walid1@gmail.com>
2017-06-08 18:39:41 +0200

    testb6

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need v1.1
+        // replace this example code with whatever you need vfin
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
